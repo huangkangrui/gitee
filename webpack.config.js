3faa1323eae4405f7a6daacb69b0a031d47bc007
huangkangrui <1505207242@qq.com>
2024-05-16 16:30:57 +0800

    feat: 项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          },
+          "less-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            // 模块小于 maxSize，会被作为Base64编码的字符串注入到包中， 
+            // 否则模块文件会被生成到输出的目标目录中
+            maxSize: 1 * 1024
+          }
+        },
+        generator: {
+          filename: 'assets/img/[name].[hash:6][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html'
+    })
+  ],
+}

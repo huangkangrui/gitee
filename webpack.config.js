277836814f8d1a96ab2ff04f67b570d620a6eff8
huangkangrui <1505207242@qq.com>
2024-05-17 16:33:45 +0800

    feat: 配置开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,15 @@ module.exports = {
     clean: true,
 
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    // 路径别名配置
+    alias: {
+      src: path.resolve(__dirname, 'src/'),
+      '@/': path.resolve(__dirname, 'src/'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   module: {
     rules: [
       {
@@ -31,17 +40,17 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
             // 模块小于 maxSize，会被作为Base64编码的字符串注入到包中， 
             // 否则模块文件会被生成到输出的目标目录中
-            maxSize: 1 * 1024
+            maxSize: 1 * 1024
           }
         },
-        generator: {
-          filename: 'assets/img/[name].[hash:6][ext]'
+        generator: {
+          filename: 'assets/img/[name].[hash:6][ext]'
         }
       }
     ]
